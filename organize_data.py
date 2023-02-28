de92d983bcf53a17fb56f29c418196f84678c556
andresgalvis29 <andres.galvis@utp.edu.co>
2023-02-28 00:36:37 -0500

    Se realizo una funcion para quitar todos los valores y referencias que tengan que ver con arriendos (ya que el programa desea ver datos de ventas)

--- organize_data.py
+++ organize_data.py
@@ -3,12 +3,18 @@ import time
 import re
 from collections import defaultdict
 
+def eliminateRent(location):
+    words = location.split()
+    word = 'ARRIENDO' in words or 'ARRIENDO,' in words
+    return word
+
 def dataOrganize(file):
     fhand = open(file,encoding='utf8')
     filecsv = csv.DictReader(fhand,delimiter=',')
     filterdata = defaultdict(list)
     for object in filecsv:
         location = object['location'].upper()
+        if eliminateRent(location) is True : continue
         price = object['price']
         price = re.sub('[$.]','',price)
         #print(location,'||',price)
@@ -17,6 +23,7 @@ def dataOrganize(file):
         prom = int(sum(filterdata[getlist])/len(filterdata[getlist]))
         filterdata[getlist] = prom
     return dict(filterdata)
+
 
 
 if __name__ == '__main__':
